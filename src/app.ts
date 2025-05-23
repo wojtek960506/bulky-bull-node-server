d04717777e40f9facad3dd735178c2c6f9ea48e3
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-05-23 17:12:03 +0200

    some cleaning of initial structure

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, { Response, Request} from 'express';
+import { routeNotFound } from './middleware/routeNotFound';
+// import mongoose from 'mongoose';
+// import { config } from './config';
+
+
+
+const app = express();
+
+
+export const start = () => {
+
+  // for processing URL-encoded form data to make it available via req.body
+  app.use(express.urlencoded())
+  // for parsing incoming requests with JSON payload to make it available via req.body
+  app.use(express.json());
+
+  // await mongoose.connect(db_connection_string)
+
+
+
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('It is time for bulky season')
+  })
+
+  app.use(routeNotFound);
+
+  app.listen(8080, () => {
+    console.log('Bulky Bull bulking on port 8080')
+  });
+};
---
+++ src/middleware/routeNotFound.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function routeNotFound(req: Request, res: Response, next: NextFunction) {
+  const msg = 'Route not found';
+  const error = new Error(msg);
+  console.error(msg);
+
+  res.status(404).json({
+    error: {
+      message: error.message
+    }
+  });
+}
