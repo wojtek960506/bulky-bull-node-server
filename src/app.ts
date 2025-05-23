a941940f08675fd1003feb6aa3d7719c7b1e7349
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-05-23 17:38:02 +0200

    added connection to mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
 import express, { Response, Request} from 'express';
 import { routeNotFound } from './middleware/routeNotFound';
-// import mongoose from 'mongoose';
-// import { config } from './config';
+import mongoose from 'mongoose';
+import { config } from './config/config';
 
 
 
 const app = express();
 
 
-export const start = () => {
+export const start = async () => {
 
   // for processing URL-encoded form data to make it available via req.body
   app.use(express.urlencoded())
   // for parsing incoming requests with JSON payload to make it available via req.body
   app.use(express.json());
 
-  // await mongoose.connect(db_connection_string)
-
-
-
+  try {
+    const connection = await mongoose.connect(config.DB_CONNECTION, config.DB_OPTIONS)
+    console.log('Connected to Mongo: ', connection.version);
+  } catch (error) {
+    console.error(error)
+  }
 
   app.get('/', (req: Request, res: Response) => {
     res.send('It is time for bulky season')
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,32 @@
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 
 dotenv.config();
 
+const PORT = Number(process.env.PORT || 1234)
+
+const DB_URL = process.env.DB_URL || '';
+const DB_ADMIN = process.env.DB_ADMIN || '';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const APP_NAME = process.env.DB_DATABASE || '';
+const DB_OPTIONS: mongoose.ConnectOptions = { 
+  retryWrites: true,
+  w: 'majority',
+  appName: APP_NAME
+};
+
+const DB_CONNECTION = `mongodb+srv://${DB_ADMIN}:${DB_PASSWORD}@${DB_URL}`
+
+
 interface Config {
-  port: number;
-  db_admin: string;
-  db_password: string;
+  PORT: number;
+  DB_OPTIONS: mongoose.ConnectOptions;
+  DB_CONNECTION: string;
 }
 
+
 export const config: Config = {
-  port: Number(process.env.PORT),
-  db_admin: process.env.DB_ADMIN || '',
-  db_password: process.env.DB_PASSWORD || '',
+  PORT,
+  DB_OPTIONS,
+  DB_CONNECTION: DB_CONNECTION,
 }
